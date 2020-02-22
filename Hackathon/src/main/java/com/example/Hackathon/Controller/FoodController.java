4c5d05a24b8929f875c25fe658e603c771096cf2
ohisme <hao.wang@emory.edu>
2020-02-23 02:04:11 +0800

    Add a test quotation

--- Hackathon/src/main/java/com/example/Hackathon/Controller/FoodController.java
+++ Hackathon/src/main/java/com/example/Hackathon/Controller/FoodController.java
@@ -15,6 +15,7 @@ import javax.validation.Valid;
 @RequestMapping("/food")
 public class FoodController {
 
+    //Test Test
     @Autowired
     private FoodService foodService;
 
--- Hackathon/src/main/java/com/example/Hackathon/Models/Food.java
+++ Hackathon/src/main/java/com/example/Hackathon/Models/Food.java
@@ -10,6 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
+import java.awt.*;
 
 @Entity
 @Data
@@ -39,4 +40,7 @@ public class Food {
     public Boolean seafood;
 
 
+    public String setName(String name) {
+        return name;
+    }
 }
