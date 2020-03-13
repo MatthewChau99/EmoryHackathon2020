647dbcdf9cc11787d3eb7c14680d8213cfead6e4
Matthew Chau <26354088+TOLOTOLO999@users.noreply.github.com>
2020-03-13 15:34:00 -0400

    Added git ignore file

--- Hackthon-master/Hackthon-master/Hackathon/src/main/java/com/example/Hackathon/Repository/FoodRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Hackathon.Repository;
-
-import com.example.Hackathon.Models.Food;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FoodRepository extends JpaRepository<Food, Long> {
-}
--- Hackthon-master/Hackthon-master/Hackathon/src/main/java/com/example/Hackathon/Repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Hackathon.Repository;
-
-import com.example.Hackathon.Models.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
